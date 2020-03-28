f840af8d11282a95e1dabc27f374f825793317bb
Sebastian Krysmanski <noreply@manski.net>
2020-03-28 16:05:14 +0100

    Added TypeExtensions

---
+++ AppWeave.Core.Tests/Tests/Extensions/TypeExtensionsTests.cs
@@ -0,0 +1,122 @@
+﻿#region License
+// Copyright 2020 - 2020 AppWeave.Core (https://github.com/skrysmanski/AppWeave.Core)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Collections.Generic;
+using System.Numerics;
+
+using AppWeave.Core.Extensions;
+
+using Shouldly;
+
+using Xunit;
+
+namespace AppWeave.Core.Tests.Extensions
+{
+    public sealed class TypeExtensionsTests
+    {
+        [Fact]
+        public void TestIsNumericType()
+        {
+            typeof(string).IsNumericType().ShouldBe(false);
+            typeof(char).IsNumericType().ShouldBe(false);
+
+            typeof(byte).IsNumericType().ShouldBe(true);
+            typeof(sbyte).IsNumericType().ShouldBe(true);
+            typeof(short).IsNumericType().ShouldBe(true);
+            typeof(ushort).IsNumericType().ShouldBe(true);
+            typeof(int).IsNumericType().ShouldBe(true);
+            typeof(uint).IsNumericType().ShouldBe(true);
+            typeof(long).IsNumericType().ShouldBe(true);
+            typeof(ulong).IsNumericType().ShouldBe(true);
+
+            typeof(float).IsNumericType().ShouldBe(true);
+            typeof(double).IsNumericType().ShouldBe(true);
+            typeof(decimal).IsNumericType().ShouldBe(true);
+
+            typeof(BigInteger).IsNumericType().ShouldBe(true);
+        }
+
+        [Fact]
+        public void TestIsNumericIntegerType()
+        {
+            typeof(string).IsNumericIntegerType().ShouldBe(false);
+            typeof(char).IsNumericIntegerType().ShouldBe(false);
+
+            typeof(byte).IsNumericIntegerType().ShouldBe(true);
+            typeof(sbyte).IsNumericIntegerType().ShouldBe(true);
+            typeof(short).IsNumericIntegerType().ShouldBe(true);
+            typeof(ushort).IsNumericIntegerType().ShouldBe(true);
+            typeof(int).IsNumericIntegerType().ShouldBe(true);
+            typeof(uint).IsNumericIntegerType().ShouldBe(true);
+            typeof(long).IsNumericIntegerType().ShouldBe(true);
+            typeof(ulong).IsNumericIntegerType().ShouldBe(true);
+
+            typeof(float).IsNumericIntegerType().ShouldBe(false);
+            typeof(double).IsNumericIntegerType().ShouldBe(false);
+            typeof(decimal).IsNumericIntegerType().ShouldBe(false);
+
+            typeof(BigInteger).IsNumericIntegerType().ShouldBe(true);
+        }
+
+        [Fact]
+        public void TestIsNumericFloatType()
+        {
+            typeof(string).IsNumericFloatType().ShouldBe(false);
+            typeof(char).IsNumericFloatType().ShouldBe(false);
+
+            typeof(byte).IsNumericFloatType().ShouldBe(false);
+            typeof(sbyte).IsNumericFloatType().ShouldBe(false);
+            typeof(short).IsNumericFloatType().ShouldBe(false);
+            typeof(ushort).IsNumericFloatType().ShouldBe(false);
+            typeof(int).IsNumericFloatType().ShouldBe(false);
+            typeof(uint).IsNumericFloatType().ShouldBe(false);
+            typeof(long).IsNumericFloatType().ShouldBe(false);
+            typeof(ulong).IsNumericFloatType().ShouldBe(false);
+
+            typeof(float).IsNumericFloatType().ShouldBe(true);
+            typeof(double).IsNumericFloatType().ShouldBe(true);
+            typeof(decimal).IsNumericFloatType().ShouldBe(true);
+
+            typeof(BigInteger).IsNumericFloatType().ShouldBe(false);
+        }
+
+        [Fact]
+        public void TestIsNullableValueType()
+        {
+            typeof(int).IsNullableValueType().ShouldBe(false);
+            typeof(int?).IsNullableValueType().ShouldBe(true);
+
+            typeof(KeyValuePair<int, int>).IsNullableValueType().ShouldBe(false);
+            typeof(KeyValuePair<int, int>?).IsNullableValueType().ShouldBe(true);
+
+            typeof(string).IsNullableValueType().ShouldBe(false);
+            typeof(object).IsNullableValueType().ShouldBe(false);
+        }
+
+        [Fact]
+        public void TestIsNullableType()
+        {
+            typeof(int).IsNullableType().ShouldBe(false);
+            typeof(int?).IsNullableType().ShouldBe(true);
+
+            typeof(KeyValuePair<int, int>).IsNullableType().ShouldBe(false);
+            typeof(KeyValuePair<int, int>?).IsNullableType().ShouldBe(true);
+
+            typeof(string).IsNullableType().ShouldBe(true);
+            typeof(object).IsNullableType().ShouldBe(true);
+        }
+    }
+}
---
+++ AppWeave.Core/Extensions/TypeExtensions.cs
@@ -0,0 +1,155 @@
+﻿#region License
+// Copyright 2020 - 2020 AppWeave.Core (https://github.com/skrysmanski/AppWeave.Core)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Numerics;
+
+using AppWeave.Core.Utils;
+
+using JetBrains.Annotations;
+
+namespace AppWeave.Core.Extensions
+{
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Returns whether this type is a (scalar) numeric type. This list includes <c>byte</c>
+        /// and <c>sbyte</c> as well as <see cref="BigInteger"/>. However <see cref="Complex"/>
+        /// is excluded.
+        /// </summary>
+        /// <remarks>
+        /// This method checks the type's full name but ignores the type's assembly. It's therefor
+        /// not 100% fool proof. However, the chance that someone recreates one of the system types
+        /// which then should not be a numeric value is rather slim (or even impossible?).
+        /// </remarks>
+        /// <seealso cref="IsNumericIntegerType"/>
+        /// <seealso cref="IsNumericFloatType"/>
+        [PublicAPI, Pure]
+        public static bool IsNumericType([NotNull] this Type type)
+        {
+            Verify.ParamNotNull(type, nameof(type));
+
+            switch (type.FullName)
+            {
+                case "System.Byte":
+                case "System.SByte":
+                case "System.Int16":
+                case "System.UInt16":
+                case "System.Int32":
+                case "System.UInt32":
+                case "System.Int64":
+                case "System.UInt64":
+                case "System.Numerics.BigInteger":
+                    return true;
+
+                case "System.Single":
+                case "System.Double":
+                case "System.Decimal":
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns whether this type is an integer type. This list includes <c>byte</c>
+        /// and <c>sbyte</c> as well as <see cref="BigInteger"/>.
+        /// </summary>
+        /// <remarks>
+        /// This method checks the type's full name but ignores the type's assembly. It's therefor
+        /// not 100% fool proof. However, the chance that someone recreates one of the system types
+        /// which then should not be a numeric value is rather slim (or even impossible?).
+        /// </remarks>
+        /// <seealso cref="IsNumericType"/>
+        /// <seealso cref="IsNumericFloatType"/>
+        [PublicAPI, Pure]
+        public static bool IsNumericIntegerType([NotNull] this Type type)
+        {
+            Verify.ParamNotNull(type, nameof(type));
+
+            switch (type.FullName)
+            {
+                case "System.Byte":
+                case "System.SByte":
+                case "System.Int16":
+                case "System.UInt16":
+                case "System.Int32":
+                case "System.UInt32":
+                case "System.Int64":
+                case "System.UInt64":
+                case "System.Numerics.BigInteger":
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns whether this type is a floating point type. This list includes <c>float</c>,
+        /// <c>double</c> and <c>decimal</c>.
+        /// </summary>
+        /// <remarks>
+        /// This method checks the type's full name but ignores the type's assembly. It's therefor
+        /// not 100% fool proof. However, the chance that someone recreates one of the system types
+        /// which then should not be a numeric value is rather slim (or even impossible?).
+        /// </remarks>
+        /// <seealso cref="IsNumericIntegerType"/>
+        /// <seealso cref="IsNumericType"/>
+        [PublicAPI, Pure]
+        public static bool IsNumericFloatType([NotNull] this Type type)
+        {
+            Verify.ParamNotNull(type, nameof(type));
+
+            switch (type.FullName)
+            {
+                case "System.Single":
+                case "System.Double":
+                case "System.Decimal":
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns whether this type can be <c>null</c>. Returns <c>true</c> for
+        /// all reference types and for all nullable value types.
+        /// </summary>
+        /// <seealso cref="IsNullableValueType"/>
+        [PublicAPI, Pure]
+        public static bool IsNullableType([NotNull] this Type type)
+        {
+            Verify.ParamNotNull(type, nameof(type));
+
+            return !type.IsValueType || type.IsNullableValueType();
+        }
+
+        /// <summary>
+        /// Returns whether this is a nullable value type.
+        /// </summary>
+        /// <seealso cref="IsNullableType"/>
+        [PublicAPI, Pure]
+        public static bool IsNullableValueType([NotNull] this Type type)
+        {
+            Verify.ParamNotNull(type, nameof(type));
+
+            return Nullable.GetUnderlyingType(type) != null;
+        }
+    }
+}
