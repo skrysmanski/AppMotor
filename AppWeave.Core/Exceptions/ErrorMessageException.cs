219c3b571ef126c37c714cdb6a25af382333d8ba
Sebastian Krysmanski <noreply@manski.net>
2020-04-04 08:29:09 +0200

    Added ErrorMessageException

---
+++ AppWeave.Core/Exceptions/ErrorMessageException.cs
@@ -0,0 +1,46 @@
+﻿#region License
+// Copyright 2020 - 2020 AppWeave.Core (https://github.com/skrysmanski/AppWeave.Core)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+
+using JetBrains.Annotations;
+
+namespace AppWeave.Core.Exceptions
+{
+    /// <summary>
+    /// An exception whose message can/should be displayed to the user - but not the stacktrace.
+    /// </summary>
+    public class ErrorMessageException : Exception
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="message">The message to display to the user.</param>
+        [PublicAPI]
+        public ErrorMessageException([NotNull] string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Only writes the message; no type nor stacktrace.
+        /// </summary>
+        public override string ToString()
+        {
+            return this.Message;
+        }
+    }
+}
