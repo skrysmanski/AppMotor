041719261a2a27a5a3ebf67dd2eb5e68b6f02fd5
Sebastian Krysmanski <noreply@manski.net>
2020-04-04 12:10:55 +0200

    Added EnumUtils

---
+++ AppWeave.Core.Tests/Tests/Utils/EnumUtilsTests.cs
@@ -0,0 +1,47 @@
+﻿#region License
+// Copyright 2020 - 2020 AppWeave.Core (https://github.com/skrysmanski/AppWeave.Core)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using AppWeave.Core.Utils;
+
+using Shouldly;
+
+using Xunit;
+
+namespace AppWeave.Core.Tests.Utils
+{
+    public sealed class EnumUtilsTests
+    {
+        [Fact]
+        public void TestGetValues()
+        {
+            // test
+            MyTestEnum[] enumValues = EnumUtils.GetValues<MyTestEnum>();
+
+            // verify
+            enumValues.ShouldContain(MyTestEnum.Value1);
+            enumValues.ShouldContain(MyTestEnum.Value2);
+            enumValues.ShouldContain(MyTestEnum.Value3);
+            enumValues.Length.ShouldBe(3);
+        }
+
+        private enum MyTestEnum
+        {
+            Value1,
+            Value2,
+            Value3,
+        }
+    }
+}
---
+++ AppWeave.Core/Utils/EnumUtils.cs
@@ -0,0 +1,37 @@
+﻿#region License
+// Copyright 2020 - 2020 AppWeave.Core (https://github.com/skrysmanski/AppWeave.Core)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+
+using JetBrains.Annotations;
+
+namespace AppWeave.Core.Utils
+{
+    public static class EnumUtils
+    {
+        /// <summary>
+        /// Returns the enum values of the specified enum.
+        /// </summary>
+        /// <remarks>
+        /// This is the typed version of <see cref="Enum.GetValues"/>.
+        /// </remarks>
+        [PublicAPI, NotNull, Pure]
+        public static TEnum[] GetValues<TEnum>() where TEnum : Enum
+        {
+            return (TEnum[])Enum.GetValues(typeof(TEnum));
+        }
+    }
+}
