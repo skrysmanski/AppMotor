a3025b67d19571d9a80f12f41f17d665be0f86fc
Sebastian Krysmanski <noreply@manski.net>
2020-10-03 12:02:52 +0200

    Added Base32Encoding

---
+++ AppMotor.Core.Tests/Tests/Utils/Base32EncodingTests.cs
@@ -0,0 +1,116 @@
+﻿#region License
+// Copyright 2020 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Text;
+
+using AppMotor.Core.Utils;
+
+using Shouldly;
+
+using Xunit;
+
+namespace AppMotor.Core.Tests.Utils
+{
+    public sealed class Base32EncodingTests
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("f", "MY======")]
+        [InlineData("fo", "MZXQ====")]
+        [InlineData("foo", "MZXW6===")]
+        [InlineData("foob", "MZXW6YQ=")]
+        [InlineData("fooba", "MZXW6YTB")]
+        [InlineData("foobar", "MZXW6YTBOI======")]
+        public void TestEncoding_WithPadding(string input, string expectedOutput)
+        {
+            var inputArray = Encoding.ASCII.GetBytes(input);
+            Base32Encoding.DefaultWithPadding.Encode(inputArray).ShouldBe(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("f", "MY")]
+        [InlineData("fo", "MZXQ")]
+        [InlineData("foo", "MZXW6")]
+        [InlineData("foob", "MZXW6YQ")]
+        [InlineData("fooba", "MZXW6YTB")]
+        [InlineData("foobar", "MZXW6YTBOI")]
+        public void TestEncoding_WithoutPadding(string input, string expectedOutput)
+        {
+            var inputArray = Encoding.ASCII.GetBytes(input);
+            Base32Encoding.DefaultWithoutPadding.Encode(inputArray).ShouldBe(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("f", "MY======")]
+        [InlineData("fo", "MZXQ====")]
+        [InlineData("foo", "MZXW6===")]
+        [InlineData("foob", "MZXW6YQ=")]
+        [InlineData("fooba", "MZXW6YTB")]
+        [InlineData("foobar", "MZXW6YTBOI======")]
+        public void TestDecoding_WithPadding(string expectedOutput, string input)
+        {
+            var expectedOutputArray = Encoding.ASCII.GetBytes(expectedOutput);
+            Base32Encoding.DefaultWithPadding.Decode(input).ShouldBe(expectedOutputArray);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("f", "MY")]
+        [InlineData("fo", "MZXQ")]
+        [InlineData("foo", "MZXW6")]
+        [InlineData("foob", "MZXW6YQ")]
+        [InlineData("fooba", "MZXW6YTB")]
+        [InlineData("foobar", "MZXW6YTBOI")]
+        public void TestDecoding_WithoutPadding(string expectedOutput, string input)
+        {
+            var expectedOutputArray = Encoding.ASCII.GetBytes(expectedOutput);
+            Base32Encoding.DefaultWithoutPadding.Decode(input).ShouldBe(expectedOutputArray);
+        }
+
+        [Fact]
+        public void TestInvalidPaddingChar()
+        {
+            Should.Throw<ArgumentException>(() => new Base32Encoding(Base32Encoding.DEFAULT_SYMBOLS, paddingChar: 'A'))
+                  .ParamName.ShouldBe("paddingChar");
+        }
+
+        [Fact]
+        public void TestInvalidSymbolCount()
+        {
+            Should.Throw<ArgumentException>(() => new Base32Encoding(Base32Encoding.DEFAULT_SYMBOLS.Substring(0, 31)))
+                  .ParamName.ShouldBe("symbols");
+            Should.Throw<ArgumentException>(() => new Base32Encoding(Base32Encoding.DEFAULT_SYMBOLS + "0"))
+                  .ParamName.ShouldBe("symbols");
+        }
+
+        [Fact]
+        public void TestDuplicatedSymbol()
+        {
+            Should.Throw<ArgumentException>(() => new Base32Encoding(Base32Encoding.DEFAULT_SYMBOLS.Replace('H', 'A')))
+                  .ParamName.ShouldBe("symbols");
+        }
+
+        [Fact]
+        public void TestDecodingInvalidChar()
+        {
+            // 0 is an invalid symbol in the default Base32 symbols list.
+            Should.Throw<FormatException>(() => Base32Encoding.DefaultWithPadding.Decode("MZX0"));
+        }
+    }
+}
---
+++ AppMotor.Core/Utils/Base32Encoding.cs
@@ -0,0 +1,419 @@
+﻿#region License
+// Copyright 2020 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+
+using JetBrains.Annotations;
+
+namespace AppMotor.Core.Utils
+{
+    /// <summary>
+    /// Represents a Base32 converter (as defined by RFC 4648) with a customizable symbol list.
+    /// </summary>
+    public class Base32Encoding
+    {
+        /// <summary>
+        /// The default Base32 symbols - as defined by RFC 4648.
+        /// </summary>
+        /// <seealso cref="DefaultWithPadding"/>
+        /// <seealso cref="DefaultWithoutPadding"/>
+        [PublicAPI]
+        public const string DEFAULT_SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+
+        /// <summary>
+        /// The default padding character - as defined by RFC 4648.
+        /// </summary>
+        /// <seealso cref="PaddingChar"/>
+        [PublicAPI]
+        public const char DEFAULT_PADDING_CHAR = '=';
+
+        /// <summary>
+        /// The Base32 converter with the default symbols (see <see cref="DEFAULT_SYMBOLS"/>) and default padding character (see <see cref="DEFAULT_PADDING_CHAR"/>).
+        /// </summary>
+        /// <seealso cref="DefaultWithoutPadding"/>
+        [PublicAPI]
+        public static Base32Encoding DefaultWithPadding { get; } = new Base32Encoding(DEFAULT_SYMBOLS, paddingChar: DEFAULT_PADDING_CHAR);
+
+        /// <summary>
+        /// The Base32 converter with the default symbols (see <see cref="DEFAULT_SYMBOLS"/>) and but without padding.
+        /// </summary>
+        /// <seealso cref="DefaultWithPadding"/>
+        [PublicAPI]
+        public static Base32Encoding DefaultWithoutPadding { get; } = new Base32Encoding(DEFAULT_SYMBOLS, paddingChar: null);
+
+        private const int BITS_PER_SYMBOL = 5;
+
+        private const int SYMBOLS_PER_GROUP = 8;
+
+        private const int BITS_PER_BYTE = 8;
+
+        private const int BYTES_PER_GROUP = 5; // = BITS_PER_SYMBOL * SYMBOLS_PER_GROUP / 8 (bits per byte)
+
+        private readonly char[] m_symbols;
+
+        private readonly Dictionary<char, byte> m_reverseSymbols;
+
+        /// <summary>
+        /// The padding character. If <c>null</c>, no padding will be used.
+        /// </summary>
+        [PublicAPI]
+        public char? PaddingChar { get; }
+
+        [PublicAPI]
+        public Base32Encoding(string symbols, char? paddingChar = DEFAULT_PADDING_CHAR)
+            : this(symbols.AsNotNullArgument(nameof(symbols)).ToCharArray(), createCopyOfSymbols: false, paddingChar)
+        {
+        }
+
+        [PublicAPI]
+        public Base32Encoding(char[] symbols, char? paddingChar = DEFAULT_PADDING_CHAR)
+            : this(symbols, createCopyOfSymbols: true, paddingChar)
+        {
+        }
+
+        private Base32Encoding(char[] symbols, bool createCopyOfSymbols, char? paddingChar)
+        {
+            Validate.Argument.IsNotNull(symbols, nameof(symbols));
+
+            if (symbols.Length != 32)
+            {
+                throw new ArgumentException($"You need to specify at 32 symbols (but only specified {symbols.Length}).", nameof(symbols));
+            }
+
+            if (createCopyOfSymbols)
+            {
+                this.m_symbols = (char[])symbols.Clone();
+            }
+            else
+            {
+                this.m_symbols = symbols;
+            }
+
+            this.m_reverseSymbols = new Dictionary<char, byte>(this.m_symbols.Length);
+            for (byte i = 0; i < this.m_symbols.Length; i++)
+            {
+                var symbol = this.m_symbols[i];
+                if (!this.m_reverseSymbols.TryAdd(symbol, i))
+                {
+                    throw new ArgumentException($"The list of symbols contains '{symbol}' multiple times.", nameof(symbols));
+                }
+            }
+
+            if (paddingChar != null)
+            {
+                if (this.m_reverseSymbols.ContainsKey(paddingChar.Value))
+                {
+                    throw new ArgumentException($"The padding character ('{paddingChar}') must not be in the list of symbols.", nameof(paddingChar));
+                }
+
+                this.PaddingChar = paddingChar;
+            }
+        }
+
+        [Pure]
+        public string Encode(ArraySegment<byte> data)
+        {
+            if (data.Count == 0)
+            {
+                return "";
+            }
+
+            var result = new StringBuilder(CalcEncodedStringLength(data.Count));
+
+            var groupStream = new ReadableBase32SymbolGroupStream(data);
+
+            while (true)
+            {
+                int readSymbols = groupStream.ReadNextGroup();
+                if (readSymbols == 0)
+                {
+                    break;
+                }
+
+                for (int i = 0; i < readSymbols; i++)
+                {
+                    var symbol = this.m_symbols[groupStream.SymbolBuffer[i]];
+                    result.Append(symbol);
+                }
+
+                if (readSymbols < SYMBOLS_PER_GROUP)
+                {
+                    if (this.PaddingChar != null)
+                    {
+                        for (int i = readSymbols; i < SYMBOLS_PER_GROUP; i++)
+                        {
+                            result.Append(this.PaddingChar.Value);
+                        }
+                    }
+
+                    break;
+                }
+            }
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Returns the length of a Base32 string based on the specified number of bytes. Note
+        /// that for simplicity reasons, we don't return a smaller number if padding is disabled.
+        /// </summary>
+        [Pure]
+        private static int CalcEncodedStringLength(int byteCount)
+        {
+            int groupCount;
+
+            if (byteCount % BYTES_PER_GROUP == 0)
+            {
+                groupCount = byteCount / BYTES_PER_GROUP;
+            }
+            else
+            {
+                groupCount = (int)Math.Ceiling((double)byteCount / BYTES_PER_GROUP);
+            }
+
+            return groupCount * SYMBOLS_PER_GROUP;
+        }
+
+        [Pure]
+        public byte[] Decode(string encodedString)
+        {
+            Validate.Argument.IsNotNull(encodedString, nameof(encodedString));
+
+            if (encodedString.Length == 0)
+            {
+                return Array.Empty<byte>();
+            }
+
+            int bufferSize = CalcDecodedByteCount(encodedString.Length);
+            byte[] sharedWriteBuffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+            byte[] symbolGroupBuffer = ArrayPool<byte>.Shared.Rent(SYMBOLS_PER_GROUP);
+            try
+            {
+                var groupStream = new WritableBase32SymbolGroupStream(sharedWriteBuffer);
+
+                int symbolGroupBufferIndex = 0;
+                foreach (var symbol in encodedString)
+                {
+                    if (symbol == this.PaddingChar)
+                    {
+                        break;
+                    }
+
+                    if (!this.m_reverseSymbols.TryGetValue(symbol, out var value))
+                    {
+                        throw new FormatException($"The symbol '{symbol}' is not a valid Base32 symbol.");
+                    }
+
+                    symbolGroupBuffer[symbolGroupBufferIndex] = value;
+
+                    if (symbolGroupBufferIndex == SYMBOLS_PER_GROUP - 1)
+                    {
+                        symbolGroupBufferIndex = 0;
+                        groupStream.WriteGroup(symbolGroupBuffer.AsSpan(0, SYMBOLS_PER_GROUP));
+                    }
+                    else
+                    {
+                        symbolGroupBufferIndex++;
+                    }
+                }
+
+                if (symbolGroupBufferIndex != 0)
+                {
+                    groupStream.WriteGroup(symbolGroupBuffer.AsSpan(0, symbolGroupBufferIndex));
+                }
+
+                var decodedBytes = new byte[groupStream.WrittenBytes];
+                sharedWriteBuffer.AsSpan(0, groupStream.WrittenBytes).CopyTo(decodedBytes);
+
+                return decodedBytes;
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(symbolGroupBuffer);
+                ArrayPool<byte>.Shared.Return(sharedWriteBuffer);
+            }
+        }
+
+        [Pure]
+        private static int CalcDecodedByteCount(int encodedStringLength)
+        {
+            int groupCount;
+
+            if (encodedStringLength % SYMBOLS_PER_GROUP == 0)
+            {
+                groupCount = encodedStringLength / SYMBOLS_PER_GROUP;
+            }
+            else
+            {
+                groupCount = (int)Math.Ceiling((double)encodedStringLength / SYMBOLS_PER_GROUP);
+            }
+
+            return groupCount * BYTES_PER_GROUP;
+        }
+
+        private sealed class ReadableBase32SymbolGroupStream
+        {
+            private const ulong BIT_MASK = (1 << BITS_PER_SYMBOL) - 1;
+
+            private readonly byte[] m_data;
+
+            private int m_offset;
+
+            private int m_count;
+
+            public readonly byte[] SymbolBuffer = new byte[SYMBOLS_PER_GROUP];
+
+            public ReadableBase32SymbolGroupStream(ArraySegment<byte> data)
+            {
+                this.m_data = data.Array ?? throw new ArgumentException("The array of this segment is null.", nameof(data));
+                this.m_offset = data.Offset;
+                this.m_count = data.Count;
+            }
+
+            public int ReadNextGroup()
+            {
+                if (this.m_count == 0)
+                {
+                    return 0;
+                }
+
+                int bytesToRead = this.m_count < BYTES_PER_GROUP ? this.m_count : BYTES_PER_GROUP;
+
+                ulong allBits = 0;
+
+                for (int i = 0; i < bytesToRead; i++)
+                {
+                    allBits |= (ulong)this.m_data[this.m_offset + i] << ((BYTES_PER_GROUP - i - 1) * BITS_PER_BYTE);
+                }
+
+                // Byte layout:
+                //
+                //      4       3        2        1        0
+                // +--------+--------+--------+--------+--------+
+                // |< 1 >< 2| >< 3 ><|.4 >< 5.|>< 6 ><.|7 >< 8 >|
+                // +--------+--------+--------+--------+--------+
+                //
+
+                int symbolCount;
+
+                this.SymbolBuffer[0] = (byte)((allBits >> ((SYMBOLS_PER_GROUP - 1) * BITS_PER_SYMBOL)) & BIT_MASK);
+                this.SymbolBuffer[1] = (byte)((allBits >> ((SYMBOLS_PER_GROUP - 2) * BITS_PER_SYMBOL)) & BIT_MASK);
+
+                if (bytesToRead > 1)
+                {
+                    this.SymbolBuffer[2] = (byte)((allBits >> ((SYMBOLS_PER_GROUP - 3) * BITS_PER_SYMBOL)) & BIT_MASK);
+                    this.SymbolBuffer[3] = (byte)((allBits >> ((SYMBOLS_PER_GROUP - 4) * BITS_PER_SYMBOL)) & BIT_MASK);
+
+                    if (bytesToRead > 2)
+                    {
+                        this.SymbolBuffer[4] = (byte)((allBits >> ((SYMBOLS_PER_GROUP - 5) * BITS_PER_SYMBOL)) & BIT_MASK);
+
+                        if (bytesToRead > 3)
+                        {
+                            this.SymbolBuffer[5] = (byte)((allBits >> ((SYMBOLS_PER_GROUP - 6) * BITS_PER_SYMBOL)) & BIT_MASK);
+                            this.SymbolBuffer[6] = (byte)((allBits >> ((SYMBOLS_PER_GROUP - 7) * BITS_PER_SYMBOL)) & BIT_MASK);
+
+                            if (bytesToRead == 5)
+                            {
+                                this.SymbolBuffer[7] = (byte)((allBits >> ((SYMBOLS_PER_GROUP - 8) * BITS_PER_SYMBOL)) & BIT_MASK);
+                                symbolCount = 8;
+                            }
+                            else
+                            {
+                                symbolCount = 7;
+                            }
+                        }
+                        else
+                        {
+                            symbolCount = 5;
+                        }
+                    }
+                    else
+                    {
+                        symbolCount = 4;
+                    }
+                }
+                else
+                {
+                    symbolCount = 2;
+                }
+
+                this.m_offset += bytesToRead;
+                this.m_count -= bytesToRead;
+
+                return symbolCount;
+            }
+        }
+
+        private sealed class WritableBase32SymbolGroupStream
+        {
+            private const int BYTE_MASK = 0xFF;
+
+            private readonly byte[] m_buffer;
+
+            public int WrittenBytes { get; private set; }
+
+            public WritableBase32SymbolGroupStream(byte[] buffer)
+            {
+                this.m_buffer = buffer;
+            }
+
+            public void WriteGroup(ReadOnlySpan<byte> symbolGroup)
+            {
+                ulong allBits = 0;
+
+                for (int i = 0; i < symbolGroup.Length; i++)
+                {
+                    allBits |= (ulong)symbolGroup[i] << ((SYMBOLS_PER_GROUP - i - 1) * BITS_PER_SYMBOL);
+                }
+
+                int byteCountToWrite;
+
+                switch (symbolGroup.Length)
+                {
+                    case 2:
+                        byteCountToWrite = 1;
+                        break;
+                    case 4:
+                        byteCountToWrite = 2;
+                        break;
+                    case 5:
+                        byteCountToWrite = 3;
+                        break;
+                    case 7:
+                        byteCountToWrite = 4;
+                        break;
+                    case 8:
+                        byteCountToWrite = 5;
+                        break;
+
+                    default:
+                        throw new InvalidOperationException($"Invalid symbol group count: {symbolGroup.Length}");
+                }
+
+                for (int i = 0; i < byteCountToWrite; i++)
+                {
+                    byte byteToWrite = (byte)((allBits >> ((BYTES_PER_GROUP - i - 1) * BITS_PER_BYTE)) & BYTE_MASK);
+                    this.m_buffer[this.WrittenBytes] = byteToWrite;
+                    this.WrittenBytes++;
+                }
+            }
+        }
+    }
+}
