a2b45285c099a5e5de6bf94463e9bae8f7914b27
Sebastian Krysmanski <noreply@manski.net>
2020-10-10 05:52:38 +0200

    Added IDateTimeProvider and DefaultDateTimeProvider

---
+++ AppMotor.Core.Tests/Tests/Utils/DefaultDateTimeProviderTests.cs
@@ -0,0 +1,43 @@
+﻿#region License
+// Copyright 2020 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Threading;
+
+using AppMotor.Core.Utils;
+
+using Shouldly;
+
+using Xunit;
+
+namespace AppMotor.Core.Tests.Utils
+{
+    public sealed class DefaultDateTimeProviderTests
+    {
+        [Fact]
+        public void TestNow()
+        {
+            DefaultDateTimeProvider.Instance.LocalNow.ShouldBe(DateTime.Now, tolerance: TimeSpan.FromMilliseconds(2));
+            DefaultDateTimeProvider.Instance.UtcNow.ShouldBe(DateTime.UtcNow, tolerance: TimeSpan.FromMilliseconds(2));
+
+            Thread.Sleep(TimeSpan.FromMilliseconds(500));
+
+            // Check that is has changed
+            DefaultDateTimeProvider.Instance.LocalNow.ShouldBe(DateTime.Now, tolerance: TimeSpan.FromMilliseconds(2));
+            DefaultDateTimeProvider.Instance.UtcNow.ShouldBe(DateTime.UtcNow, tolerance: TimeSpan.FromMilliseconds(2));
+        }
+    }
+}
---
+++ AppMotor.Core/Utils/IDateTimeProvider.cs
@@ -0,0 +1,44 @@
+﻿#region License
+// Copyright 2020 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+
+using JetBrains.Annotations;
+
+namespace AppMotor.Core.Utils
+{
+    /// <summary>
+    /// Provides the current time and date. This interface primarily exists for (unit) testing
+    /// code that relies on the current time and date (thus allowing test code to mock the current
+    /// time and date). For the default implementation, see <see cref="DefaultDateTimeProvider"/>.
+    /// </summary>
+    public interface IDateTimeProvider
+    {
+        /// <summary>
+        /// The current date and time in the local timezone (i.e. <see cref="DateTimeKind.Local"/>).
+        /// </summary>
+        /// <seealso cref="UtcNow"/>
+        [PublicAPI]
+        DateTime LocalNow { get; }
+
+        /// <summary>
+        /// The current date and time the UTC timezone (i.e. <see cref="DateTimeKind.Utc"/>).
+        /// </summary>
+        /// <seealso cref="LocalNow"/>
+        [PublicAPI]
+        DateTime UtcNow { get; }
+    }
+}
