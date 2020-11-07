4e54876bc583da3bedf8b4221bd99e62036d1a72
Sebastian Krysmanski <noreply@manski.net>
2020-11-07 05:57:33 +0100

    Provided translation for ConsoleApplication

--- AppMotor.Core/System/ConsoleApplication.cs
+++ AppMotor.Core/System/ConsoleApplication.cs
@@ -1,12 +1,12 @@
 ﻿#region License
 // Copyright 2020 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ using System.Diagnostics;
 
 using AppMotor.Core.Exceptions;
 using AppMotor.Core.Logging;
+using AppMotor.Core.Properties;
 using AppMotor.Core.Utils;
 
 using JetBrains.Annotations;
@@ -74,7 +75,7 @@ namespace AppMotor.Core.System
             if ((Debugger.IsAttached || app.WaitForKeyPressOnExit) && !Terminal.IsInputRedirected)
             {
                 Terminal.WriteLine();
-                Terminal.WriteLine("Press any key to exit...");
+                Terminal.WriteLine(LocalizableResources.PressAnyKeyToExit);
                 Terminal.ReadKey();
             }
 
