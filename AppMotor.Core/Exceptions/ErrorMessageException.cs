0950f2bea908f531c524e30aaf39a626b40be216
Sebastian Krysmanski <noreply@manski.net>
2020-11-18 16:33:06 +0100

    Marked "message" parameter in ErrorMessageException as localizable

--- AppMotor.Core/Exceptions/ErrorMessageException.cs
+++ AppMotor.Core/Exceptions/ErrorMessageException.cs
@@ -1,12 +1,12 @@
 ﻿#region License
 // Copyright 2020 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,7 @@
 #endregion
 
 using System;
+using System.ComponentModel;
 
 using JetBrains.Annotations;
 
@@ -30,7 +31,7 @@ namespace AppMotor.Core.Exceptions
         /// </summary>
         /// <param name="message">The message to display to the user.</param>
         [PublicAPI]
-        public ErrorMessageException(string message)
+        public ErrorMessageException([Localizable(true)] string message)
             : base(message)
         {
         }
