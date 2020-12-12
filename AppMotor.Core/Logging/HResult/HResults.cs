593deb9ee05f51133f7cd23d52ca698c2b4efa45
Sebastian Krysmanski <noreply@manski.net>
2020-12-12 08:51:56 +0100

    Restructured header of HResults.cs and updated its contents from upstream

--- AppMotor.Core/Logging/HResult/HResults.cs
+++ AppMotor.Core/Logging/HResult/HResults.cs
@@ -1,6 +1,12 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+﻿// Source: https://github.com/dotnet/runtime/blob/master/src/libraries/Common/src/System/HResults.cs
+
+// ReSharper disable All
+
+//=============================================================================
+
+
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 //=============================================================================
 //
@@ -9,9 +15,6 @@
 //
 //
 //===========================================================================*/
-
-// Source: https://github.com/dotnet/runtime/blob/master/src/libraries/Common/src/System/HResults.cs
-// ReSharper disable All
 
 namespace System
 {
