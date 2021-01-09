f2293c9446d3ede908e2c9dd52e8398fa47574ea
Sebastian Krysmanski <skrysmanski@users.noreply.github.com>
2021-01-09 11:36:34 +0100

    Add samples for CliApplication (#8)

---
+++ samples/AppMotor.CliApp.Samples.AppWithVerbs/AddCommand.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+
+using AppMotor.CliApp.CommandLine;
+using AppMotor.CliApp.Terminals;
+
+namespace AppMotor.CliApp.Samples.AppWithVerbs
+{
+    internal class AddCommand : GitCommandBase
+    {
+        /// <inheritdoc />
+        public override string HelpText => "Add file contents to the index";
+
+        /// <inheritdoc />
+        protected override CliCommandExecutor Executor => new(Execute);
+
+        private CliParam<bool> DryRunParam { get; } = new("--dry-run", "-n")
+        {
+            HelpText = "Don’t actually add the file(s), just show if they exist and/or will be ignored.",
+        };
+
+        private CliParam<string?> ChmodParam { get; } = new("--chmod")
+        {
+            DefaultValue = null,
+            HelpText = "Override the executable bit of the added files. The executable bit is only changed in the index, the files on disk are left unchanged.",
+        };
+
+        private CliParam<FileInfo[]> FilesParam { get; } = new("files", positionIndex: 0)
+        {
+            HelpText = "The files to add.",
+        };
+
+        private void Execute()
+        {
+            if (this.QuietParam.Value)
+            {
+                Terminal.WriteLine((TextInDarkGray)"This command would run quite.");
+                Terminal.WriteLine();
+            }
+
+            if (this.VerboseParam.Value)
+            {
+                Terminal.WriteLine((TextInDarkGray)"This command would run verbose.");
+                Terminal.WriteLine();
+            }
+
+            if (this.DryRunParam.Value)
+            {
+                Terminal.WriteLine("Just a dry-run.");
+                Terminal.WriteLine();
+            }
+
+            if (this.ChmodParam.Value is null)
+            {
+                Terminal.WriteLine("This command would add the following files:");
+            }
+            else
+            {
+                Terminal.WriteLine($"This command would add the following files (with chmod={this.ChmodParam.Value}):");
+            }
+            Terminal.WriteLine();
+
+            foreach (var fileToAdd in this.FilesParam.Value)
+            {
+                Terminal.WriteLine((TextInCyan)$" - {fileToAdd.FullName}");
+            }
+        }
+    }
+}
---
+++ samples/AppMotor.CliApp.Samples.AppWithVerbs/Program.cs
@@ -0,0 +1,25 @@
+﻿using AppMotor.CliApp.CommandLine;
+
+namespace AppMotor.CliApp.Samples.AppWithVerbs
+{
+    /// <summary>
+    /// This sample provides a command line interface similar to Git's CLI.
+    /// </summary>
+    internal static class Program
+    {
+        private static int Main(string[] args)
+        {
+            var app = new CliApplicationWithVerbs()
+            {
+                AppDescription = ".NET wrapper around Git (for demonstration purposes). The commands are non-functional.",
+                Verbs = new[]
+                {
+                    new CliVerb("clone", new CloneCommand()),
+                    new CliVerb("add", new AddCommand()),
+                },
+            };
+
+            return app.Run(args);
+        }
+    }
+}
