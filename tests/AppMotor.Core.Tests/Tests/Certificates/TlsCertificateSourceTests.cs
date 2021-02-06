216d3f80dead424aed5687e5ad1efaedf05eaf21
Sebastian Krysmanski <noreply@manski.net>
2021-02-06 05:47:26 +0100

    Added first TlsCertificateSource test

---
+++ tests/AppMotor.Core.Tests/Tests/Certificates/TlsCertificateSourceTests.cs
@@ -0,0 +1,41 @@
+﻿#region License
+// Copyright 2021 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using AppMotor.Core.Certificates;
+
+using Microsoft.VisualBasic;
+
+using Shouldly;
+
+using Xunit;
+
+namespace AppMotor.Core.Tests.Certificates
+{
+    public sealed class TlsCertificateSourceTests
+    {
+        [Fact]
+        public void Test_ImportPem()
+        {
+            // Test
+            var source = TlsCertificateSource.FromFile("Tests/Certificates/cert-files/cert.pem", "Tests/Certificates/cert-files/key.pem");
+            var cert = new TlsCertificate(source);
+
+            // Validate
+            cert.HasPrivateKey.ShouldBe(true);
+            cert.SubjectName.Name.ShouldBe("CN=www.example.com, OU=Org, O=Company Name, L=Portland, S=Oregon, C=US");
+        }
+    }
+}
