ed1472336302aa2b58b826b503014921498f144f
Sebastian Krysmanski <noreply@manski.net>
2021-06-27 07:07:06 +0200

    Added Generic Host sample

---
+++ samples/AppMotor.CliApp.Hosting.Sample/MyGenericHostCliCommand.cs
@@ -0,0 +1,16 @@
+﻿using AppMotor.CliApp.CommandLine.Hosting;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AppMotor.CliApp.Hosting.Sample
+{
+    internal sealed class MyGenericHostCliCommand : GenericHostCliCommand
+    {
+        /// <inheritdoc />
+        protected override void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddHostedService<MyTestServer>();
+        }
+    }
+}
