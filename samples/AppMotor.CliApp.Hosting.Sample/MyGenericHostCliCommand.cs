d62f2dfa28cb22c84289fe85ff55078ab46a48cf
Sebastian Krysmanski <noreply@manski.net>
2021-07-03 11:13:43 +0200

    Changed GenericHost sample a little bit

--- samples/AppMotor.CliApp.Hosting.Sample/MyGenericHostCliCommand.cs
+++ samples/AppMotor.CliApp.Hosting.Sample/MyGenericHostCliCommand.cs
@@ -1,4 +1,5 @@
-﻿using AppMotor.CliApp.CommandLine.Hosting;
+﻿using AppMotor.CliApp.CommandLine;
+using AppMotor.CliApp.CommandLine.Hosting;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,9 +9,19 @@ namespace AppMotor.CliApp.Hosting.Sample
     internal sealed class MyGenericHostCliCommand : GenericHostCliCommand
     {
         /// <inheritdoc />
+        protected override CliCommandExecutor ExplicitExecutor => new(Execute);
+
+        /// <inheritdoc />
         protected override void ConfigureServices(HostBuilderContext context, IServiceCollection services)
         {
             services.AddHostedService<MyTestServer>();
+        }
+
+        private void Execute()
+        {
+            this.Terminal.Write("Enter something: ");
+            var text = this.Terminal.ReadLine();
+            this.Terminal.WriteLine($"You wrote: {text}");
         }
     }
 }
