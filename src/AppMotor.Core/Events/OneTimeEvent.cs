3fda1b5723694256edd48df2f556b313ef2737f2
Sebastian Krysmanski <noreply@manski.net>
2021-07-04 12:33:32 +0200

    Implemented OneTimeEvent

---
+++ src/AppMotor.Core/Events/OneTimeEvent.cs
@@ -0,0 +1,127 @@
+﻿#region License
+// Copyright 2021 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+using JetBrains.Annotations;
+
+namespace AppMotor.Core.Events
+{
+    /// <summary>
+    /// An event that will only happen one time (and then never again). Primary examples of such an event
+    /// are lifetime event (e.g. application started, application stopped).
+    /// </summary>
+    public class OneTimeEvent<TEventArgs>
+    {
+        private readonly object _eventRaiseLock = new();
+
+        private bool _hasBeenRaised;
+
+        private readonly Event<TEventArgs> _event;
+
+        private readonly EventRaiser<TEventArgs> _eventRaiser;
+
+        /// <summary>
+        /// Whether this event has been raised or not.
+        /// </summary>
+        // ReSharper disable once InconsistentlySynchronizedField
+        public bool HasBeenRaised => this._hasBeenRaised;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        [PublicAPI]
+        protected OneTimeEvent()
+        {
+            (this._event, this._eventRaiser) = Event<TEventArgs>.Create();
+        }
+
+        /// <summary>
+        /// Creates an event and its event raiser.
+        /// </summary>
+        [MustUseReturnValue]
+        [SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "By design")]
+        public static (OneTimeEvent<TEventArgs>, EventRaiser<TEventArgs>) Create()
+        {
+            var oneTimeEvent = new OneTimeEvent<TEventArgs>();
+
+            return (oneTimeEvent, new EventRaiser<TEventArgs>(oneTimeEvent.RaiseEvent));
+        }
+
+        /// <summary>
+        /// Registers a synchronous event handler and returns its registration. Returns <c>null</c> if the event has
+        /// already been raised/fired.
+        /// </summary>
+        /// <param name="eventHandler">The event handler</param>
+        /// <returns>The event handler registration. Dispose this instance to remove the registration.</returns>
+        /// <seealso cref="RegisterEventHandler(Func{TEventArgs,Task})"/>
+        public IEventHandlerRegistration? RegisterEventHandler(Action<TEventArgs> eventHandler)
+        {
+            lock (this._eventRaiseLock)
+            {
+                if (this._hasBeenRaised)
+                {
+                    // Event has been raised
+                    return null;
+                }
+                else
+                {
+                    return this._event.RegisterEventHandler(eventHandler);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Registers an <c>async</c> event handler and returns its registration. Returns <c>null</c> if the event has
+        /// already been raised/fired.
+        /// </summary>
+        /// <param name="eventHandler">The event handler</param>
+        /// <returns>The event handler registration. Dispose this instance to remove the registration.</returns>
+        /// <seealso cref="RegisterEventHandler(Action{TEventArgs})"/>
+        public IEventHandlerRegistration? RegisterEventHandler(Func<TEventArgs, Task> eventHandler)
+        {
+            lock (this._eventRaiseLock)
+            {
+                if (this._hasBeenRaised)
+                {
+                    // Event has been raised
+                    return null;
+                }
+                else
+                {
+                    return this._event.RegisterEventHandler(eventHandler);
+                }
+            }
+        }
+
+        private async Task RaiseEvent(TEventArgs eventArgs)
+        {
+            lock (this._eventRaiseLock)
+            {
+                if (this._hasBeenRaised)
+                {
+                    return;
+                }
+
+                this._hasBeenRaised = true;
+            }
+
+            await this._eventRaiser.RaiseEventAsync(eventArgs).ConfigureAwait(false);
+        }
+    }
+}
---
+++ tests/AppMotor.Core.Tests/Tests/Events/OneTimeEventTests.cs
@@ -0,0 +1,115 @@
+﻿#region License
+// Copyright 2021 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Threading.Tasks;
+
+using AppMotor.Core.Events;
+
+using Shouldly;
+
+using Xunit;
+
+namespace AppMotor.Core.Tests.Events
+{
+    public sealed class OneTimeEventTests
+    {
+        private readonly Guid _testGuid = Guid.NewGuid();
+
+        private int _syncHandlerCallCount;
+
+        private int _asyncHandlerCallCount;
+
+        [Fact]
+        public void Test_BasicUsage()
+        {
+            var (@event, eventRaiser) = OneTimeEvent<TestEventArgs>.Create();
+
+            @event.HasBeenRaised.ShouldBe(false);
+
+            var syncRegistration = @event.RegisterEventHandler(OnTestEvent);
+            syncRegistration.ShouldNotBeNull();
+            var asyncRegistration = @event.RegisterEventHandler(OnTestEventAsync);
+            asyncRegistration.ShouldNotBeNull();
+
+            eventRaiser.RaiseEvent(new TestEventArgs(this._testGuid));
+
+            this._syncHandlerCallCount.ShouldBe(1);
+            this._asyncHandlerCallCount.ShouldBe(1);
+            @event.HasBeenRaised.ShouldBe(true);
+
+            eventRaiser.RaiseEvent(new TestEventArgs(this._testGuid));
+
+            this._syncHandlerCallCount.ShouldBe(1);
+            this._asyncHandlerCallCount.ShouldBe(1);
+            @event.HasBeenRaised.ShouldBe(true);
+
+            @event.RegisterEventHandler(OnTestEvent).ShouldBeNull();
+            @event.RegisterEventHandler(OnTestEventAsync).ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Test_BasicUsage_Async()
+        {
+            var (@event, eventRaiser) = OneTimeEvent<TestEventArgs>.Create();
+
+            @event.HasBeenRaised.ShouldBe(false);
+
+            var syncRegistration = @event.RegisterEventHandler(OnTestEvent);
+            syncRegistration.ShouldNotBeNull();
+            var asyncRegistration = @event.RegisterEventHandler(OnTestEventAsync);
+            asyncRegistration.ShouldNotBeNull();
+
+            await eventRaiser.RaiseEventAsync(new TestEventArgs(this._testGuid));
+
+            this._syncHandlerCallCount.ShouldBe(1);
+            this._asyncHandlerCallCount.ShouldBe(1);
+            @event.HasBeenRaised.ShouldBe(true);
+
+            await eventRaiser.RaiseEventAsync(new TestEventArgs(this._testGuid));
+
+            this._syncHandlerCallCount.ShouldBe(1);
+            this._asyncHandlerCallCount.ShouldBe(1);
+            @event.HasBeenRaised.ShouldBe(true);
+
+            @event.RegisterEventHandler(OnTestEvent).ShouldBeNull();
+            @event.RegisterEventHandler(OnTestEventAsync).ShouldBeNull();
+        }
+
+        private void OnTestEvent(TestEventArgs eventArgs)
+        {
+            eventArgs.TestGuid.ShouldBe(this._testGuid);
+            this._syncHandlerCallCount++;
+        }
+
+        private async Task OnTestEventAsync(TestEventArgs eventArgs)
+        {
+            await Task.Delay(0);
+            eventArgs.TestGuid.ShouldBe(this._testGuid);
+            this._asyncHandlerCallCount++;
+        }
+
+        private sealed class TestEventArgs
+        {
+            public Guid TestGuid { get; }
+
+            public TestEventArgs(Guid testGuid)
+            {
+                this.TestGuid = testGuid;
+            }
+        }
+    }
+}
