8d339d2540a63defb78de0e993f1e49cbc6b1cae
Sebastian Krysmanski <noreply@manski.net>
2021-10-25 13:18:48 +0200

    Improved test for DelaySafe()

--- tests/AppMotor.Core.Tests/Tests/Utils/TaskUtilsTests.cs
+++ tests/AppMotor.Core.Tests/Tests/Utils/TaskUtilsTests.cs
@@ -21,6 +21,8 @@ using System.Threading.Tasks;
 using AppMotor.Core.Utils;
 using AppMotor.TestCore.Extensions;
 
+using Shouldly;
+
 using Xunit;
 
 namespace AppMotor.Core.Tests.Utils
@@ -28,12 +30,19 @@ namespace AppMotor.Core.Tests.Utils
     public sealed class TaskUtilsTests
     {
         [Fact]
-        public async Task Test_DelaySafe()
+        public async Task Test_DelaySafe_NotCanceled()
         {
-            await TaskUtils.DelaySafe(TimeSpan.FromSeconds(0.2), CancellationToken.None).OrTimeoutAfter(TimeSpan.FromSeconds(0.5));
+            var result = await TaskUtils.DelaySafe(TimeSpan.FromSeconds(0.2), CancellationToken.None).OrTimeoutAfter(TimeSpan.FromSeconds(2));
+            result.ShouldBe(true);
+        }
 
+        [Fact]
+        public async Task Test_DelaySafe_Canceled()
+        {
             using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(0.2));
-            await TaskUtils.DelaySafe(TimeSpan.FromSeconds(20), cts.Token).OrTimeoutAfter(TimeSpan.FromSeconds(0.5));
+
+            var result = await TaskUtils.DelaySafe(TimeSpan.FromSeconds(20), cts.Token).OrTimeoutAfter(TimeSpan.FromSeconds(2));
+            result.ShouldBe(false);
         }
     }
 }
