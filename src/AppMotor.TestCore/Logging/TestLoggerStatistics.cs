3532eee4612af6346e7071b09befc498ec0906db
Sebastian Krysmanski <noreply@manski.net>
2021-10-29 12:38:52 +0200

    Improved implementation of TestLoggerStatistics

--- src/AppMotor.TestCore/Logging/TestLoggerStatistics.cs
+++ src/AppMotor.TestCore/Logging/TestLoggerStatistics.cs
@@ -14,6 +14,10 @@
 // limitations under the License.
 #endregion
 
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
 using Microsoft.Extensions.Logging;
 
 using Shouldly;
@@ -25,51 +29,52 @@ namespace AppMotor.TestCore.Logging
     /// </summary>
     public sealed class TestLoggerStatistics
     {
-        private readonly object _lock = new();
+        private readonly Dictionary<LogLevel, int> _logEntryCounts = new();
 
         /// <summary>
-        /// The number of warnings that were logged.
+        /// Returns the number of log entries for the given log level.
         /// </summary>
-        public int WarningCount { get; private set; }
+        [Pure]
+        public int GetLogCount(LogLevel logLevel)
+        {
+            lock (this._logEntryCounts)
+            {
+                if (this._logEntryCounts.TryGetValue(logLevel, out var count))
+                {
+                    return count;
+                }
 
-        /// <summary>
-        /// The number of errors that were logged.
-        /// </summary>
-        public int ErrorCount { get; private set; }
-
-        /// <summary>
-        /// The number of critical messages that were logged.
-        /// </summary>
-        public int CriticalCount { get; private set; }
+                return 0;
+            }
+        }
 
         internal void OnLogMessage(LogLevel logLevel)
         {
-            lock (this._lock)
+            lock (this._logEntryCounts)
             {
-                switch (logLevel)
-                {
-                    case LogLevel.Critical:
-                        this.CriticalCount++;
-                        break;
-
-                    case LogLevel.Error:
-                        this.ErrorCount++;
-                        break;
-
-                    case LogLevel.Warning:
-                        this.WarningCount++;
-                        break;
-                }
+                this._logEntryCounts[logLevel] = GetLogCount(logLevel) + 1;
             }
         }
 
         /// <summary>
-        /// Ensures that both <see cref="ErrorCount"/> and <seealso cref="CriticalCount"/> are 0.
+        /// Ensures that no entries with log level <see cref="LogLevel.Error"/> or <see cref="LogLevel.Critical"/> have
+        /// been logged.
         /// </summary>
         public void ShouldHaveNoErrors()
         {
-            this.ErrorCount.ShouldBe(0);
-            this.CriticalCount.ShouldBe(0);
+            GetLogCount(LogLevel.Error).ShouldBe(0);
+            GetLogCount(LogLevel.Critical).ShouldBe(0);
+        }
+
+        /// <summary>
+        /// Resets this instance (i.e. the count for all log levels is reset to 0).
+        /// </summary>
+        public void Reset()
+        {
+            lock (this._logEntryCounts)
+            {
+                this._logEntryCounts.Clear();
+            }
         }
     }
 }
