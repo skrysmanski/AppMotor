b054914f3f5d74d05b9cf9a857edde54c001113f
Sebastian Krysmanski <noreply@manski.net>
2022-04-21 08:44:20 +0200

    Some improvements in MvcStartup

--- src/AppMotor.HttpServer/Startups/MvcStartup.cs
+++ src/AppMotor.HttpServer/Startups/MvcStartup.cs
@@ -29,7 +29,7 @@ namespace AppMotor.HttpServer.Startups;
 
 /// <summary>
 /// Startup class for this ASP.NET Core MVC application (with Razor views). The idea behind this
-/// class is to make bootstrapping an MVC application extremely easier by providing the most common
+/// class is to make bootstrapping an MVC application extremely easy by providing the most common
 /// defaults out-of-the-box.
 /// </summary>
 public class MvcStartup : IAspNetStartup
@@ -57,6 +57,14 @@ public class MvcStartup : IAspNetStartup
     public string ErrorPagePath { get; init; } = "/Home/Error";
 
     /// <summary>
+    /// Constructor. Uses <see cref="Assembly.GetEntryAssembly"/> as main assembly (i.e. where controllers and views are located).
+    /// </summary>
+    public MvcStartup()
+        : this(Assembly.GetEntryAssembly() ?? throw new InvalidOperationException("Could not determine main assembly."))
+    {
+    }
+
+    /// <summary>
     /// Constructor.
     /// </summary>
     /// <param name="mainAssembly">The assembly where all the controllers and views are located.</param>
@@ -71,11 +79,10 @@ public class MvcStartup : IAspNetStartup
         var mvcBuilder = services.AddControllersWithViews();
 
         //
-        // Enable views and controllers from the main assembly.
+        // Enable views and controllers from the main assembly. (Otherwise, they won't be found.)
         //
-        // See: https://docs.microsoft.com/en-us/aspnet/core/mvc/advanced/app-parts?view=aspnetcore-5.0#load-aspnet-core-features
+        // See: https://docs.microsoft.com/en-us/aspnet/core/mvc/advanced/app-parts?#load-aspnet-core-features
         //
-        // Enables controllers from this assembly
         mvcBuilder.AddApplicationPart(this._mainAssembly);
 
         // Load the precompiled Razor views - if they exist.
