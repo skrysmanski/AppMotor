5e861be6abd21bc6cc7425e50e0732579d8e72bf
Sebastian Krysmanski <noreply@manski.net>
2022-04-21 06:06:21 +0200

    Renamed HTTP server sample

--- samples/AppMotor.HttpServer.MvcSample/Program.cs
+++ samples/AppMotor.HttpServer.MvcSample/Program.cs
@@ -2,14 +2,15 @@
 using AppMotor.HttpServer;
 using AppMotor.HttpServer.Sample.Services;
 
-Terminal.WriteLine("Try the following URLs:");
+Terminal.WriteLine("Try the following URLs in your browser:");
 Terminal.WriteLine();
+Terminal.WriteLine(" - http://localhost:1234");
 Terminal.WriteLine(" - http://localhost:1234/api/httpsport");
 Terminal.WriteLine(" - http://localhost:1234/api/ping");
 Terminal.WriteLine();
 
 var app = new HttpServerApplication(port: 1234);
 
-app.Services.AddSingleton(new ServiceSettingsProvider(4567));
+app.Services.AddSingleton(new ServiceSettingsProvider(httpsPort: 4567));
 
 return app.Run();
