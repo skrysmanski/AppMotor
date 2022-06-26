8d7bfbbf412464442e7f099d93ca367b136c33d7
Sebastian Krysmanski <noreply@manski.net>
2022-06-26 07:19:52 +0200

    Implemented HexColor

---
+++ src/AppMotor.Core/Colors/HexColor.cs
@@ -0,0 +1,161 @@
+﻿#region License
+// Copyright 2022 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Globalization;
+using System.Text;
+
+using AppMotor.Core.Utils;
+
+namespace AppMotor.Core.Colors;
+
+/// <summary>
+/// A color in HTML/CSS/Internet hex notation; e.g. <c>#FFAACC</c>. Supports
+/// both 3 and 4 component colors (i.e. with or without alpha component) as
+/// well as components with two characters (e.g. <c>#FFAACC</c>) or components
+/// with one character (e.g. <c>#FAC</c>).
+/// </summary>
+public readonly struct HexColor : IColor, IEquatable<HexColor>
+{
+    private readonly RgbColor _color;
+
+    /// <inheritdoc />
+    public byte A => this._color.A;
+
+    /// <summary>
+    /// This color in (normalized) hex string notation; e.g. <c>#FFDF00C2</c>
+    /// </summary>
+    public string HexString => $"#{this._color.A:X2}{this._color.R:X2}{this._color.G:X2}{this._color.B:X2}";
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public HexColor(string hexColor)
+    {
+        Validate.ArgumentWithName(nameof(hexColor)).IsNotNullOrEmpty(hexColor);
+
+        int start = hexColor[0] == '#' ? 1 : 0;
+        var colorAsSpan = hexColor.AsSpan(start);
+
+        switch (colorAsSpan.Length)
+        {
+            // Single digit hex color with 3 components (e.g. "#fac")
+            case 3:
+            {
+                var sb = new StringBuilder(capacity: 6);
+                sb.Append(colorAsSpan[0], repeatCount: 2);
+                sb.Append(colorAsSpan[1], repeatCount: 2);
+                sb.Append(colorAsSpan[2], repeatCount: 2);
+                colorAsSpan = sb.ToString();
+                break;
+            }
+
+            // Single digit hex color with 4 components (e.g. "#bfac")
+            case 4:
+            {
+                var sb = new StringBuilder(capacity: 6);
+                sb.Append(colorAsSpan[0], repeatCount: 2);
+                sb.Append(colorAsSpan[1], repeatCount: 2);
+                sb.Append(colorAsSpan[2], repeatCount: 2);
+                sb.Append(colorAsSpan[3], repeatCount: 2);
+                colorAsSpan = sb.ToString();
+                break;
+            }
+
+            case 6:
+            case 8:
+                // Ok
+                break;
+
+            default:
+                throw new ArgumentException($"The string '{hexColor}' is not a valid hex color string.");
+        }
+
+        // ReSharper disable once IdentifierTypo
+        uint argb;
+
+        if (colorAsSpan.Length == 6)
+        {
+            if (!uint.TryParse(colorAsSpan, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out uint rgb))
+            {
+                throw new ArgumentException($"The string '{hexColor}' is not a valid hex color string.");
+            }
+            argb = rgb + 0xFF000000;
+        }
+        else
+        {
+            if (!uint.TryParse(colorAsSpan, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out argb))
+            {
+                throw new ArgumentException($"The string '{hexColor}' is not a valid hex color string.");
+            }
+        }
+
+        this._color = new RgbColor((int)argb);
+    }
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public HexColor(RgbColor color)
+    {
+        this._color = color;
+    }
+
+    /// <inheritdoc />
+    public bool Equals(HexColor other)
+    {
+        return this._color.Equals(other._color);
+    }
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        return obj is HexColor other && Equals(other);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return this._color.GetHashCode();
+    }
+
+    /// <summary>
+    /// == operator
+    /// </summary>
+    public static bool operator ==(HexColor left, HexColor right)
+    {
+        return left.Equals(right);
+    }
+
+    /// <summary>
+    /// != operator
+    /// </summary>
+    public static bool operator !=(HexColor left, HexColor right)
+    {
+        return !left.Equals(right);
+    }
+
+    /// <inheritdoc />
+    public RgbColor ToRgb()
+    {
+        return this._color;
+    }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return this.HexString;
+    }
+}
---
+++ tests/AppMotor.Core.Tests/Tests/Colors/HexColorTests.cs
@@ -0,0 +1,123 @@
+﻿#region License
+// Copyright 2022 AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Drawing;
+
+using AppMotor.Core.Colors;
+using AppMotor.TestCore;
+
+using Shouldly;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AppMotor.Core.Tests.Colors;
+
+public sealed class HexColorTests : TestBase
+{
+    public HexColorTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+    {
+    }
+
+    [Theory]
+    [InlineData("#FFFF0000", 255, 255,   0,   0)] // red
+    [InlineData("8000ff00",  128,   0, 255,   0)] // green
+    [InlineData("#000F",       0,   0,   0, 255)] // blue
+    [InlineData("f000",      255,   0,   0,   0)] // black
+    [InlineData("FFF",       255, 255, 255, 255)] // white
+    [InlineData("#ccc",      255, 204, 204, 204)] // light gray
+    [InlineData("#ffff69B4", 255, 255, 105, 180)] // hot pink
+    [InlineData("#80404060", 128,  64,  64,  96)] // dark blue
+    [InlineData("#00734220",   0, 115,  66,  32)] // brown
+    public void Test_Conversion(string hexString, byte a, byte r, byte g, byte b)
+    {
+        // Test
+        var colorToTest = new HexColor(hexString);
+
+        var rgbColor = colorToTest.ToRgb();
+
+        // Verify
+        colorToTest.A.ShouldBe(a);
+        rgbColor.A.ShouldBe(a);
+        rgbColor.R.ShouldBe(r);
+        rgbColor.G.ShouldBe(g);
+        rgbColor.B.ShouldBe(b);
+
+        // Test 2
+        var colorToTest2 = new HexColor(rgbColor);
+
+        // Verify 2
+        colorToTest2.ToRgb().ShouldBe(rgbColor);
+        colorToTest2.HexString.ShouldBe(colorToTest.HexString);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("aa")]
+    [InlineData("aaaaa")]
+    [InlineData("aaaaaaa")]
+    [InlineData("aaaaaaaaa")]
+    [InlineData("#XXX")]
+    [InlineData("XXXXXXXX")]
+    public void Test_InvalidHexStrings(string input)
+    {
+        var ex = Should.Throw<ArgumentException>(() => new HexColor(input));
+
+        // For manual verification
+        this.TestConsole.WriteLine(ex.Message);
+    }
+
+    [Theory]
+    [InlineData("#FFFF0000", "#FFFF0000")] // red
+    [InlineData("8000FF00",  "#8000FF00")] // green
+    [InlineData("#000F",     "#000000FF")] // blue
+    [InlineData("F000",      "#FF000000")] // black
+    [InlineData("FFF",       "#FFFFFFFF")] // white
+    [InlineData("#CCC",      "#FFCCCCCC")] // light gray
+    [InlineData("#FFFF69B4", "#FFFF69B4")] // hot pink
+    [InlineData("#80404060", "#80404060")] // dark blue
+    [InlineData("#00734220", "#00734220")] // brown
+    public void Test_HexStringAndToString(string input, string expectedHexString)
+    {
+        var colorFromUpperCase = new HexColor(input);
+        colorFromUpperCase.HexString.ShouldBe(expectedHexString);
+        colorFromUpperCase.ToString().ShouldBe(expectedHexString);
+
+        var colorFromLowerCase = new HexColor(input.ToLowerInvariant());
+        colorFromLowerCase.HexString.ShouldBe(expectedHexString);
+        colorFromLowerCase.ToString().ShouldBe(expectedHexString);
+    }
+
+    [Fact]
+    public void Test_Equals()
+    {
+        var red1 = new HexColor("#f00");
+        var red2 = new HexColor(Color.Red);
+        var green = new HexColor(Color.Green);
+
+        EqualityMembersTests.TestEquals(red1, red2, green);
+    }
+
+    [Fact]
+    public void Test_GetHashCode()
+    {
+        var red1 = new HexColor("#ff0000");
+        var red2 = new HexColor(Color.Red);
+
+        EqualityMembersTests.TestGetHashCode(red1, red2);
+    }
+}
