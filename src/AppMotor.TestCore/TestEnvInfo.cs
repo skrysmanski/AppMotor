669a4fd88a7f0201287fad8a5be6bbd6483277c8
Sebastian Krysmanski <noreply@manski.net>
2022-08-28 08:48:14 +0200

    Add documentation to RunsInCiPipeline

--- src/AppMotor.TestCore/TestEnvInfo.cs
+++ src/AppMotor.TestCore/TestEnvInfo.cs
@@ -12,6 +12,9 @@ public static class TestEnvInfo
     /// Whether the tests are executed in a CI pipeline. Use this to increase timeouts
     /// when tests fail due to high load in the CI pipeline.
     /// </summary>
+    /// <remarks>
+    /// To make this property <c>true</c>, you need to define the environment variable "RUNS_IN_CI" and set its value to "true".
+    /// </remarks>
     public static bool RunsInCiPipeline => s_runsInCiPipelineLazy.Value;
 
     private static readonly Lazy<bool> s_runsInCiPipelineLazy = new(() => Environment.GetEnvironmentVariable("RUNS_IN_CI") == "true");
