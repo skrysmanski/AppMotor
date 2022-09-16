a64a1c76242ad98c323674acb1a260ac289df82e
Sebastian Krysmanski <noreply@manski.net>
2022-09-16 09:22:28 +0200

    Add test

---
+++ tests/AppMotor.CliApp.Tests/Tests/CommandLine/Utils/CommandLineConsoleTests.cs
@@ -0,0 +1,23 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+
+using AppMotor.CliApp.CommandLine.Utils;
+using AppMotor.CliApp.Terminals;
+
+using Shouldly;
+
+using Xunit;
+
+namespace AppMotor.CliApp.Tests.CommandLine.Utils;
+
+/// <summary>
+/// Tests for <see cref="CommandLineConsole"/>.
+/// </summary>
+public sealed class CommandLineConsoleTests
+{
+    [Fact]
+    public void Test_FromTerminal_RealTerminal()
+    {
+        CommandLineConsole.FromTerminal(Terminal.Instance).ShouldBe(null);
+    }
+}
