248321c0fb051ae67bdcab033ac253feb8b9ce1f
Sebastian Krysmanski <noreply@manski.net>
2022-09-16 08:00:46 +0200

    Stabilize test for CI pipeline

--- tests/AppMotor.Core.Tests/Tests/Utils/DefaultDateTimeProviderTests.cs
+++ tests/AppMotor.Core.Tests/Tests/Utils/DefaultDateTimeProviderTests.cs
@@ -2,6 +2,7 @@
 // Copyright AppMotor Framework (https://github.com/skrysmanski/AppMotor)
 
 using AppMotor.Core.Utils;
+using AppMotor.TestCore;
 using AppMotor.TestCore.Shouldly;
 
 using Shouldly;
@@ -15,13 +16,13 @@ public sealed class DefaultDateTimeProviderTests
     [Fact]
     public void TestNow()
     {
-        DefaultDateTimeProvider.Instance.LocalNow.ShouldBe(DateTime.Now, tolerance: TimeSpan.FromMilliseconds(2));
-        DefaultDateTimeProvider.Instance.UtcNow.ShouldBe(DateTimeUtc.Now, tolerance: TimeSpan.FromMilliseconds(2));
+        DefaultDateTimeProvider.Instance.LocalNow.ShouldBe(DateTime.Now, tolerance: TimeSpan.FromMilliseconds(TestEnvInfo.RunsInCiPipeline ? 50 : 2));
+        DefaultDateTimeProvider.Instance.UtcNow.ShouldBe(DateTimeUtc.Now, tolerance: TimeSpan.FromMilliseconds(TestEnvInfo.RunsInCiPipeline ? 50 : 2));
 
         Thread.Sleep(TimeSpan.FromMilliseconds(500));
 
         // Check that is has changed
-        DefaultDateTimeProvider.Instance.LocalNow.ShouldBe(DateTime.Now, tolerance: TimeSpan.FromMilliseconds(2));
-        DefaultDateTimeProvider.Instance.UtcNow.ShouldBe(DateTimeUtc.Now, tolerance: TimeSpan.FromMilliseconds(2));
+        DefaultDateTimeProvider.Instance.LocalNow.ShouldBe(DateTime.Now, tolerance: TimeSpan.FromMilliseconds(TestEnvInfo.RunsInCiPipeline ? 50 : 2));
+        DefaultDateTimeProvider.Instance.UtcNow.ShouldBe(DateTimeUtc.Now, tolerance: TimeSpan.FromMilliseconds(TestEnvInfo.RunsInCiPipeline ? 50 : 2));
     }
 }
