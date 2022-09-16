3260f283eb3c6cf26913d539cc46ae611435a309
Sebastian Krysmanski <noreply@manski.net>
2022-09-16 07:42:25 +0200

    Add StringBuilderExtensions

---
+++ src/AppMotor.Core/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+
+using System.Text;
+
+using AppMotor.Core.Utils;
+
+namespace AppMotor.Core.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="StringBuilder"/>.
+/// </summary>
+public static class StringBuilderExtensions
+{
+    /// <summary>
+    /// Appends the specified line terminator to the end of this string builder.
+    /// </summary>
+    /// <seealso cref="AppendLineLf"/>
+    /// <see cref="AppendLineCrLf"/>
+    public static StringBuilder AppendLine(this StringBuilder stringBuilder, LineTerminators lineTerminator)
+    {
+        return stringBuilder.Append(lineTerminator.AsString());
+    }
+
+    /// <summary>
+    /// Appends the Linux/Unix line terminator to the end of this string builder.
+    /// </summary>
+    /// <seealso cref="AppendLineCrLf"/>
+    /// <see cref="AppendLine"/>
+    public static StringBuilder AppendLineLf(this StringBuilder stringBuilder)
+    {
+        return stringBuilder.Append('\n');
+    }
+
+    /// <summary>
+    /// Appends the Windows line terminator to the end of this string builder.
+    /// </summary>
+    /// <seealso cref="AppendLineLf"/>
+    /// <seealso cref="AppendLine"/>
+    public static StringBuilder AppendLineCrLf(this StringBuilder stringBuilder)
+    {
+        return stringBuilder.Append("\r\n");
+    }
+}
---
+++ tests/AppMotor.Core.Tests/Tests/Extensions/StringBuilderExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+
+using System.Text;
+
+using AppMotor.Core.Extensions;
+using AppMotor.Core.Utils;
+
+using Shouldly;
+
+using Xunit;
+
+namespace AppMotor.Core.Tests.Extensions;
+
+/// <summary>
+/// Tests for <see cref="StringBuilderExtensions"/>.
+/// </summary>
+public sealed class StringBuilderExtensionsTests
+{
+    [Theory]
+    [InlineData(LineTerminators.SystemDefault)]
+    [InlineData(LineTerminators.CrLf)]
+    [InlineData(LineTerminators.Lf)]
+    public void Test_AppendLine(LineTerminators lineTerminator)
+    {
+        new StringBuilder().AppendLine(lineTerminator).ToString().ShouldBe(lineTerminator.AsString());
+    }
+
+    [Fact]
+    public void Test_AppendLineLf()
+    {
+        new StringBuilder().AppendLineLf().ToString().ShouldBe("\n");
+    }
+
+    [Fact]
+    public void Test_AppendLineCrLf()
+    {
+        new StringBuilder().AppendLineCrLf().ToString().ShouldBe("\r\n");
+    }
+}
