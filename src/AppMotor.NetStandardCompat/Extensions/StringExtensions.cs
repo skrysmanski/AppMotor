0864caa32c88afcfeed4db3420123eb24b9c65a6
Sebastian Krysmanski <noreply@manski.net>
2022-09-17 13:34:57 +0200

    Update file header

--- src/AppMotor.NetStandardCompat/Extensions/StringExtensions.cs
+++ src/AppMotor.NetStandardCompat/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿// SPDX-License-Identifier: MIT
+// Copyright AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+
+using System.Diagnostics.CodeAnalysis;
 
 using JetBrains.Annotations;
 
