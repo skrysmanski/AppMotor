179b582a26fc4251d4ac0fbbbfbe5011d606f0e2
Sebastian Krysmanski <noreply@manski.net>
2023-11-12 05:31:29 +0100

    Add UninitializedStructPropertyException

---
+++ src/AppMotor.Core/Exceptions/UninitializedStructPropertyException.cs
@@ -0,0 +1,14 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+
+using JetBrains.Annotations;
+
+namespace AppMotor.Core.Exceptions;
+
+/// <summary>
+/// Thrown if you try to access properties of an uninitialized struct.
+/// </summary>
+[PublicAPI]
+public class UninitializedStructPropertyException : InvalidOperationException
+{
+}
