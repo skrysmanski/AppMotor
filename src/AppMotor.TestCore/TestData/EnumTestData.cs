8b2c4ac1a05081848184bca34e70003c164115b1
Sebastian Krysmanski <noreply@manski.net>
2024-06-02 11:09:40 +0200

    Extract common test data class code into TestDataBase

--- src/AppMotor.TestCore/TestData/EnumTestData.cs
+++ src/AppMotor.TestCore/TestData/EnumTestData.cs
@@ -1,8 +1,6 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright AppMotor Framework (https://github.com/skrysmanski/AppMotor)
 
-using System.Collections;
-
 using AppMotor.Core.Utils;
 
 namespace AppMotor.TestCore.TestData;
@@ -10,20 +8,14 @@ namespace AppMotor.TestCore.TestData;
 /// <summary>
 /// Provides all members/values of an enum as test data to be used as <c>[ClassData(typeof(EnumTestData&lt;MyEnum&gt;))]</c>.
 /// </summary>
-public sealed class EnumTestData<TEnum> : IEnumerable<object[]> where TEnum : Enum
+public sealed class EnumTestData<TEnum> : TestDataBase where TEnum : Enum
 {
     /// <inheritdoc />
-    public IEnumerator<object[]> GetEnumerator()
+    public override IEnumerator<object[]> GetEnumerator()
     {
         foreach (var enumValue in EnumUtils.GetValues<TEnum>())
         {
             yield return [enumValue];
         }
-    }
-
-    /// <inheritdoc />
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
     }
 }
---
+++ src/AppMotor.TestCore/TestData/TestDataBase.cs
@@ -0,0 +1,21 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright AppMotor Framework (https://github.com/skrysmanski/AppMotor)
+
+using System.Collections;
+
+namespace AppMotor.TestCore.TestData;
+
+/// <summary>
+/// Base class to test data providers for xUnit. To use a test data class, attribute
+/// the test method with <c>[ClassData(typeof(...))]</c>.
+/// </summary>
+public abstract class TestDataBase : IEnumerable<object?[]>
+{
+    /// <summary>
+    /// Provides the test data. Each item in the enumeration is a separate test case
+    /// and each array element is the value for a parameter.
+    /// </summary>
+    public abstract IEnumerator<object?[]> GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
