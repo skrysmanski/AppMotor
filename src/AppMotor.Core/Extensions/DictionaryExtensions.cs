86c7bd5f4eeea18844136cce957b3a09b8d015b3
Sebastian Krysmanski <noreply@manski.net>
2024-06-29 09:16:22 +0200

    Fix typo

--- src/AppMotor.Core/Extensions/DictionaryExtensions.cs
+++ src/AppMotor.Core/Extensions/DictionaryExtensions.cs
@@ -16,7 +16,7 @@ public static class DictionaryExtensions
     /// <summary>
     /// Like <see cref="CollectionExtensions.AddRange{T}"/> but uses
     /// the indexer of the dictionary rather than <see cref="IDictionary{TKey,TValue}.Add(TKey,TValue)"/>.
-    /// Thus this method will not result in an exception if the key already exists.
+    /// Thus, this method will not result in an exception if the key already exists.
     /// </summary>
     /// <exception cref="CollectionIsReadOnlyException">Thrown if the collection is read-only.</exception>
     [PublicAPI]
